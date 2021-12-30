79819ad715a7deebaccfc79607e3b2fcc75c268f
Breanne <BreMarotta@gmail.com>
2021-12-29 20:19:01 -0500

    add styling, create button, add first eventListener

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript!";
 document.querySelector("body").appendChild(h2);
+
+
+const button =document.querySelector("button").addEventListener("click", foo)
+function foo(){
+    alert("I've been clicked!")
+}
