c003bd64763fc62258188ba0b20008c2f0cff670
Breanne <BreMarotta@gmail.com>
2021-12-29 21:03:30 -0500

    create variable btns for each fandom

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript!";
-document.querySelector("body").appendChild(h2);
+//My Buttons: will eventually use these to sort collection by fandom!
+const marvel=document.getElementById("marvelBtn")
+const starWars=document.getElementById("starWarsBtn")
+const office=document.getElementById("theOfficeBtn")
+const lego=document.getElementById("legoBtn")
+const tmnt=document.getElementById("tmntBtn")
+const harryPotter=document.getElementById("harryPotterBtn")
+const disney=document.getElementById("disneyBtn")
+const lotr=document.getElementById("lotrBtn")
+console.log(lotr)
 
 
 const button =document.querySelector("button").addEventListener("click", foo)
