303148525e519eeea479580ce03f2db122e6c8b9
Breanne <BreMarotta@gmail.com>
2021-12-29 21:27:28 -0500

    add btns to fandomPics as group

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 //My Buttons: will eventually use these to sort collection by fandom!
-const marvel=document.getElementById("marvelBtn")
-const starWars=document.getElementById("starWarsBtn")
-const office=document.getElementById("theOfficeBtn")
-const lego=document.getElementById("legoBtn")
-const tmnt=document.getElementById("tmntBtn")
-const harryPotter=document.getElementById("harryPotterBtn")
-const disney=document.getElementById("disneyBtn")
-const lotr=document.getElementById("lotrBtn")
-console.log(lotr)
-
+const btns =document.getElementsByClassName("fandomPics");
+    for(const btn of btns){
+        btn.addEventListener("click", foo)
+    }
 
 const button =document.querySelector("button").addEventListener("click", foo)
 function foo(){
-    alert("I've been clicked!")
+    console.log(this.id)
 }
+
