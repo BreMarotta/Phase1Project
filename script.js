cc1d199481a29f54a28db39c50d4354be7a1c07b
Breanne <BreMarotta@gmail.com>
2021-12-29 21:47:21 -0500

    add alerts to event listeners explaining their eventual functions

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-//My Buttons: will eventually use these to sort collection by fandom!
+//placeholder functions before I add Fetch and more functionality
+function foo(){
+    console.log(this.id)
+    alert("Eventually, each of these buttons will sort the entire collection and only show cards form the selected fandom")
+}
+function foo1(){
+    alert("This will open up a form to add a new Funko Pop card to the collection")
+}
+function foo2(){
+    alert("When user clicks this button, collection should reset, showing all cards")
+}
+
+//Fandom Buttons: will eventually use these to sort collection by fandom!
 const btns =document.getElementsByClassName("fandomPics");
     for(const btn of btns){
         btn.addEventListener("click", foo)
     }
-
-const button =document.querySelector("button").addEventListener("click", foo)
-function foo(){
-    console.log(this.id)
-}
+const resetCollection =document.querySelector("img.logo").addEventListener("click", foo2)
+const button =document.querySelector("button").addEventListener("click", foo1)
 
