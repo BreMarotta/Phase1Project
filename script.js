29c197f61cf2df69a70c9d6ce9358c94f179812f
Breanne <BreMarotta@gmail.com>
2021-12-30 22:23:44 -0500

    format form and adjust position

--- script.js
+++ script.js
@@ -9,12 +9,23 @@ function foo1(){
 function foo2(){
     alert("When user clicks this button, collection should reset, showing all cards")
 }
-
+function foo3(){
+    alert("Maybe use this feature to show who funko pop was from?")
+}
 //Fandom Buttons: will eventually use these to sort collection by fandom!
 const btns =document.getElementsByClassName("fandomPics");
     for(const btn of btns){
         btn.addEventListener("click", foo)
     }
+//Reset Button: Will reset collection to include all cards
 const resetCollection =document.querySelector("img.logo").addEventListener("click", foo2)
+
+//Should open form to add new Funko Pop to collection
 const button =document.querySelector("button").addEventListener("click", foo1)
+
+const cards =document.querySelectorAll("img.card")
+    for(const card of cards){
+        card.addEventListener("mouseover", foo3)
+    }
+
 
