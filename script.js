bab0c76f72783f8135ee4d7f60ac0dbb79220d16
Breanne <BreMarotta@gmail.com>
2022-01-03 13:10:49 -0500

    add initial Fetch request and code to display collection from json

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+document.addEventListener('DOMContentLoaded', init);
+function init(){
+    fetch(`http://localhost:3000/funkoPops`)
+    .then(response => response.json())
+    .then(pops => {
+        for(const pop of pops){
+        displayCollection(pop);
+    }
+    })
+}
 //placeholder functions before I add Fetch and more functionality
 function foo(){
     console.log(this.id)
@@ -9,8 +19,22 @@ function foo1(){
 function foo2(){
     alert("When user clicks this button, collection should reset, showing all cards")
 }
-function foo3(){
-    alert("Maybe use this feature to show who funko pop was from?")
+//function foo3(){
+//     alert("Maybe use this feature to show who funko pop was from?")
+// }
+
+function displayCollection(pop){
+    console.log(pop)
+    let card =document.createElement('p')
+    card.className = "card"
+    card.innerHTML = `
+        <h2>${pop.name}<br><h4>${pop.fandom}</h4><h2>
+
+        <img src="${pop.image}" class="img"/>
+        <h5>From: ${pop.from}</h5>
+        `
+    document.querySelector('#funko-pop-collection').appendChild(card)
+
 }
 //Fandom Buttons: will eventually use these to sort collection by fandom!
 const btns =document.getElementsByClassName("fandomPics");
@@ -23,9 +47,9 @@ const resetCollection =document.querySelector("img.logo").addEventListener("clic
 //Should open form to add new Funko Pop to collection
 const button =document.querySelector("button").addEventListener("click", foo1)
 
-const cards =document.querySelectorAll("img.card")
-    for(const card of cards){
-        card.addEventListener("mouseover", foo3)
-    }
+// //const cards =document.querySelectorAll("img.card")
+//     for(const card of cards){
+//         card.addEventListener("mouseover", foo3)
+//     }
 
 
