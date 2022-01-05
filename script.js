3a660b7946175fada3f2e08702804b44de8abc96
Breanne <BreMarotta@gmail.com>
2022-01-05 04:09:04 -0500

    reset function works to display full collection after displaying filtered collection

--- script.js
+++ script.js
@@ -23,30 +23,11 @@ function createPopCard(pop){
         `
     document.querySelector('#funko-pop-collection').appendChild(card)
 }
-const allPops = document.getElementsByClassName('card') 
-console.log(allPops)
-//WORKS!!!Attempting to create function to only display filtered cards for each fandom
-const btns =document.getElementsByClassName("fandomPics");
-for(const btn of btns){
-    btn.addEventListener("click", (e) => {
-        for(const card of allPops){
-            card.style.display= "inline-grid";
-             let fandom =card.children[1].innerText;
-            if(e.target.id !== fandom){
-                card.style.display= "none";
-            } 
-        } 
-    })
-}
-    
-    function foo(e){
-        console.log("Got to function", e.target)
-        let popsList = document.getElementsByClassName('#funko-pop-collection')
-            console.log(popsList)
-    }
+
+
 //WORKS Submits form to add new Funko Pop to collection
 const formSubmit =document.querySelector("button").addEventListener("click", addNew)
-//Function to add new Funko Pop to the collect (posts to db.json and creates new card)
+//WORKS! Function to add new Funko Pop to the collect (posts to db.json and creates new card)
 function addNew(e){
     e.preventDefault()
     fetch(`http://localhost:3000/funkoPops`, {
@@ -66,27 +47,24 @@ function addNew(e){
     .then(newPop => createPopCard(newPop))
     document.querySelector('form').reset();
 }
-
-//Function to sort and view Pops from a single fandom
-
-
-
-function resetCollection(){
-    alert("When user clicks this button, collection should reset, showing all cards")
+const allPops = document.getElementsByClassName('card') 
+//WORKS!!!Attempting to create function to only display filtered cards for each fandom
+const btns =document.getElementsByClassName("fandomPics");
+for(const btn of btns){
+    btn.addEventListener("click", (e) => {
+        for(const card of allPops){
+            card.style.display= "inline-grid";
+             let fandom =card.children[1].innerText;
+            if(e.target.id !== fandom){
+                card.style.display= "none";
+            } 
+        } 
+    })
 }
-
-
-
-//Fandom Buttons: will eventually use these to sort collection by fandom!
-
 //Reset Button: Will reset collection to include all cards
-const resetButton =document.querySelector("img.logo").addEventListener("click", resetCollection)
-
-
-
-// //const cards =document.querySelectorAll("img.card")
-//     for(const card of cards){
-//         card.addEventListener("mouseover", foo3)
-//     }
-
+const resetButton =document.querySelector("img.logo").addEventListener("click", () => {
+    for(const card of allPops){
+        card.style.display= "inline-grid";
+    }
+})
 
